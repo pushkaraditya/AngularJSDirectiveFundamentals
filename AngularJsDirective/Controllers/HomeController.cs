c37a7c7176b39e495910865c2d42716bec9983bd
Pushkar Aditya <pushkar.aditya@askiitians.com>
2016-05-31 16:06:40 +0530

    2.6 completed but 2.7 is not working out for me

---
+++ AngularJsDirective/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularJsDirective.Controllers
+{
+  public class HomeController : Controller
+  {
+    // GET: Hme
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
---
+++ AngularJsDirective/app/script.js
@@ -0,0 +1,103 @@
+﻿(function () {
+  var mainCtrl = function ($scope) {
+    $scope.message = "Hello Angular";
+    $scope.user1 = {
+      name: "Luke Skywalker",
+      address: {
+        street: 'PO Box 123',
+        city: 'Street Rebel Base',
+        planet: 'Yavin 4'
+      },
+      friends: [
+        'Han',
+        'Leia',
+        'Chewbacca'
+      ]
+    };
+    $scope.user2 = {
+      name: "Han Solo",
+      address: {
+        street: 'PO Box 123',
+        city: 'Mos Eisley',
+        planet: 'Tattoine'
+      },
+      friends: [
+        'Luke',
+        'Leia',
+        'Chewbacca'
+      ]
+    };
+  };
+
+  var app = angular.module("app");
+  app.controller("mainCtrl", mainCtrl);
+
+  app.directive("userInfoCard", function () {
+    return {
+      templateUrl: "templates/userInfoCard.html",
+      restrict: "E",
+      scope: {
+        user: '=person',
+        initialCollapsed: '@collapsed'
+      },
+      controller: function ($scope) {
+        $scope.collapsed = ($scope.initialCollapsed === 'true');
+        $scope.knightMe = function (user) {
+          user.rank = "Knight";
+        };
+        $scope.collapse = function () {
+          $scope.collapsed = !$scope.collapsed;
+        };
+      }
+    };
+  });
+
+  app.directive('address', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/address.html',
+      scope: {
+        address: '='
+      },
+      controller: function ($scope) {
+        $scope.collapsed = false;
+        $scope.expandAddress = function () {
+          $scope.collapsed = false;
+        };
+        $scope.collapseAddress = function () {
+          $scope.collapsed = true;
+        };
+        $scope.remove = function () {
+          alert();
+          //var idx = $scope.user.friends.indexOf(friend);
+          //if (idx >= -1) {
+          //  $scope.user.friends.splice(idx, 1);
+          //}
+        };
+      }
+    };
+  });
+
+  app.directive("removeFriend", function () {
+    return {
+      templateUrl: 'templates/removeFriend.html',
+      restrict: 'E',
+      scope: {
+        method: '&'
+      },
+      controller: function ($scope) {
+        $scope.removing = false;
+        $scope.startRemove = function () {
+          $scope.removing = true;
+        };
+        $scope.cancelRemove = function () {
+          $scope.removing = false;
+        };
+        $scope.confirmRemove = function () {
+          $scope.method();
+        };
+      }
+    };
+  });
+
+}());
