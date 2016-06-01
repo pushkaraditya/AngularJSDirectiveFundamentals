c81eb570ee3a60d7ae80d7b64dfb221fdb3dbedd
Pushkar Aditya <pushkar.aditya@askiitians.com>
2016-06-01 12:10:43 +0530

    3.2 incomplete save pointer

--- AngularJsDirective/app/videoCtrl.js
+++ AngularJsDirective/app/videoCtrl.js
@@ -1,10 +1,32 @@
 ﻿(function () {
   var videoCtrl = function ($scope) {
     $scope.title = "Video Spacebar directive demo";
+    $scope.messages = [];
+    $scope.handlePause = function () {
+      $scope.messages.push({ text: 'paused!' });
+      console.log('paused!');
+    };
   };
 
   var app = angular.module('app')
     .controller('videoCtrl', videoCtrl);
+
+  app.directive('eventPause', function () {
+    return {
+      restrict: 'A',
+      scope: {
+        eventPause: '&'
+      },
+      link: function (scope, el, attrs) {
+        el.on('pause', function (event) {
+          //scope.eventPause();
+          scope.$apply(function () {
+            scope.eventPause();
+          });
+        });
+      }
+    };
+  });
 
   app.directive('spacebarSupport', function () {
     return {
