edd725737fe6ef51fe21ef9071cf86b5436b1864
Pushkar Aditya <pushkar.aditya@askiitians.com>
2016-06-01 11:28:07 +0530

    completed 3.1 link function

--- AngularJsDirective/app/videoCtrl.js
+++ AngularJsDirective/app/videoCtrl.js
@@ -1,7 +1,24 @@
 ﻿(function () {
   var videoCtrl = function ($scope) {
-    $scope.message = "Message from controller";
+    $scope.title = "Video Spacebar directive demo";
   };
-  var app = angular.module('app');
-  app.controller('videoCtrl', videoCtrl);
+
+  var app = angular.module('app')
+    .controller('videoCtrl', videoCtrl);
+
+  app.directive('spacebarSupport', function () {
+    return {
+      restrict: 'A',
+      link: function (scope, el, attrs) {
+        var vidEl = el[0];
+        $('body').on('keypress', function (evt) {
+          if (evt.keyCode === 32)
+            if (vidEl.paused)
+              vidEl.play();
+            else
+              vidEl.pause();
+        });
+      }
+    };
+  });
 }());
