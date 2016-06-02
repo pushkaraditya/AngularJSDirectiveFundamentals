20f954abee1ec447e760bd6b381034686e735677
Pushkar Aditya <pushkar.aditya@askiitians.com>
2016-06-02 09:46:45 +0530

    go to previous page implementation

---
+++ AngularJsDirective/app/previous-page.js
@@ -0,0 +1,11 @@
+﻿(function () {
+  angular.module("app").directive("previousPage", function ($window) {
+    return {
+      link: function (scope, el, attrs) {
+        el.on("click", function () {
+          $window.history.back();
+        });
+      }
+    };
+  });
+}());
